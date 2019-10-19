4b470030b0dc5936be808c741202f5285cfe718e
yangyiqin-tsinghua <53291365+yangyiqin-tsinghua@users.noreply.github.com>
2019-10-19 12:09:40 +0000

    Delete cxbz.py

--- Chinese essays/grammar error/cxbz.py
+++
@@ -1,9 +0,0 @@
-import jieba.posseg as pseg
-for line in open("train_words.txt", "r"):
-    words = pseg.cut(line)   #词性标注
-    with open("train_pseg.txt", "a") as f:
-        for word, flag in words:
-            if flag != 'x':
-                f.write(flag + ' ')
-        f.write('\n')
-    f.close()
